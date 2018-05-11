e16952d9ac40a48938208d1eb10d433085f8e95b
rotem friedman <rotem.fri@campus.technion.ac.il>
2018-05-11 18:48:29 +0300

    dai

--- map_mtm.c
+++ map_mtm.c
@@ -16,19 +16,35 @@ struct Map_t{
 };
 
 
+
+//Sets the internal iterator (also called current key element) to
+//the first key element in the map
+MapKeyElement mapGetFirst(Map map) {
+    if (map == NULL) {
+        return NULL;
+    } else if (map->first_pointer == NULL) {          //check if map is empty;
+        return NULL;
+    }
+    map->iterator = map->first_pointer;              //set the iterator in the first node;
+    NodeResult *status;
+    return nodeReturnKey(map->first_pointer,status);
+}
+
+
+
 //this function get a map. Advances the map iterator to the next key element and returns the key element
 MapKeyElement mapGetNext(Map map){
-    if (map == NULL)
-        return NULL;
-    MapKeyElement next_key;
-    NodeResult node_status;
-    Node node_next=nodeGetNextIteration(map->iterator, &node_status);
-    if( node_status == NODE_NULL_PTR)
-        return NULL;
-    map->iterator=node_next;
-    next_key= nodeReturnKey(node_next,&node_status);
-    if(node_status == NODE_NULL_PTR)
-        return NULL;
-    return next_key;
+if (map == NULL)
+return NULL;
+MapKeyElement next_key;
+NodeResult node_status;
+Node node_next=nodeGetNextIteration(map->iterator, &node_status);
+if( node_status == NODE_NULL_PTR)
+return NULL;
+map->iterator=node_next;
+next_key= nodeReturnKey(node_next,&node_status);
+if(node_status == NODE_NULL_PTR)
+return NULL;
+return next_key;
 }
 
