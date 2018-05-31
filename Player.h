03f4944e5f13bae2784c6d940d9e617acd517de8
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-05-31 19:10:59 +0300

    begin mtmflixxxxxxxxxxxxxxxxx

---
+++ Player.h
@@ -0,0 +1,4 @@
+#ifndef HW4_PLAYER_H
+#define HW4_PLAYER_H
+
+#endif //HW4_PLAYER_H
--- main.cpp
+++ main.cpp
@@ -2,4 +2,7 @@
 // Created by rotem on 31/05/2018.
 //
 
-shakkkkkkkkkkkkkkkkkkkkes
+int main(){
+
+    return 0;
+}
