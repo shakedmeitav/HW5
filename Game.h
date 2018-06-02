4c7c42bdc23cfa0a15365075a9b06af4589156d1
shaked meitav <shakedmeitav@campus.technion.ac.il>
2018-06-02 10:50:25 +0300

    begin mtmflixxxxxxxxxxxxxxxxx

---
+++ Game.h
@@ -0,0 +1,19 @@
+#ifndef HW4_GAME_H
+#define HW4_GAME_H
+
+#include "Weapon.h"
+#include "Player.h"
+
+class Player {
+    char* name;
+    int level;
+    int life;
+    int strength;
+    Weapon weapon;
+    int position_of_player;
+
+
+public:
+
+
+#endif //HW4_GAME_H
