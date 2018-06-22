cc6ba02ab7e684b07c70bc3698d7bd0f7b5bb6a8
rotem friedman <rotem.fri@campus.technion.ac.il>
2018-06-22 13:16:34 +0300

    JJJ

--- Game.cpp
+++ Game.cpp
@@ -22,6 +22,7 @@ Game::Game(const Game& game): maxPlayer(game.maxPlayer),
                               array_player(new Player* [maxPlayer]){
 }
 
+
 GameStatus Game::addPlayer(const char *playerName, const char *weaponName,
                            Target target, int hit_strength) {
 
